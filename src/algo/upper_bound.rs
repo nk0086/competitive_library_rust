0cb62ffdec92e3f24957abef4352a4a6a5058f5a
nk0086 <n172586k@gmail.com>
2022-05-08 15:43:04 +0900

    Add test and generics in upper_bound

--- src/algo/upper_bound.rs
+++ src/algo/upper_bound.rs
@@ -1,7 +1,7 @@
 use cargo_snippet::snippet;
 
 #[snippet]
-fn upper_bound(array: &Vec<u64>, k: u64) -> u64 {
+fn upper_bound<T: PartialOrd>(array: &[T], k: T) -> u64 {
     let mut ok = array.len() as i64;
     let mut ng = -1;
 
@@ -22,18 +22,31 @@ mod test {
     use super::*;
     #[test]
     fn simple() {
+        let v = [0, 1, 2, 2, 3, 4, 5];
+        assert_eq!(upper_bound(&v, 2), 4);
+    }
+
+    #[test]
+    fn simple_vector() {
         let v = vec![0, 1, 2, 2, 3, 4, 5];
         assert_eq!(upper_bound(&v, 2), 4);
     }
 
     #[test]
-    fn u_max() {
+    fn simple_char() {
+        let v = ['a', 'b', 'c', 'c', 'd', 'e', 'f'];
+        assert_eq!(upper_bound(&v, 'c'), 4);
+    }
+
+    #[test]
+    fn simple_max() {
         let v = vec![1, 2, 3];
         assert_eq!(upper_bound(&v, 5), 3);
     }
 
     #[test]
-    fn l_max() {
-        let v = vec![1, 2, 3];
+    fn simple_min() {
+        let v = vec![4, 4, 5, 7, 8];
+        assert_eq!(upper_bound(&v, 3), 0);
     }
 }
